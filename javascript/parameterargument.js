9d6a47bf4d93ada029e729fa52b004304376180d
charlittamdr <charlittameidira@gmail.com>
2024-01-25 13:49:03 +0700

    parameterargument

---
+++ javascript/parameterargument.js
@@ -0,0 +1,8 @@
+function luasPersegi(s) {
+    return s * s; // Menghitung luas persegi dengan rumus sisi * sisi
+  }
+  
+  // Contoh penggunaan fungsi dengan sisi 4
+  let sisi = 4;
+  console.log("Luas persegi dengan sisi", sisi, "adalah:", luasPersegi(sisi));
+  
